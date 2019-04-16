b3a904917f1973c7b64fa41cce8b9daf46626c64
User <User@DEV-022780.OnBase.net>
2019-04-16 17:57:31 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace htc_abdulalhusaini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
